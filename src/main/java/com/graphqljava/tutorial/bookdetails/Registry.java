b41fa9e106f983e2264179a6feee7fd5def15f01
Ninad T <ninad.tungare@gmail.com>
2020-12-24 21:44:02 -0600

    switch to jetty

--- src/main/java/com/graphqljava/tutorial/bookdetails/Registry.java
+++ src/main/java/com/graphqljava/tutorial/bookdetails/Registry.java
@@ -6,29 +6,45 @@ import org.dataloader.DataLoaderRegistry;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.Nonnull;
 import java.util.Map;
 
 @Component
 public class Registry {
 
     @Bean
-    public DataLoaderRegistry dataLoaderRegistry(final GraphQLDataFetchers graphQLDataFetchers) {
-
-        final DataLoaderRegistry registry = new DataLoaderRegistry();
-
+    @Nonnull
+    public DataLoader<String, Map<String, String>> authorDataLoader(
+            @Nonnull final GraphQLDataFetchers graphQLDataFetchers
+    ) {
         final BatchLoader<String, Map<String, String>> authorBatchLoader =
                 graphQLDataFetchers.authorBatchLoader();
-        final DataLoader<String, Map<String, String>> authorDataLoader =
-                DataLoader.newDataLoader(authorBatchLoader);
 
+        return DataLoader.newDataLoader(authorBatchLoader);
+    }
+
+    @Bean
+    @Nonnull
+    public DataLoader<String, Map<String, String>> characterDataLoader(
+            @Nonnull final GraphQLDataFetchers graphQLDataFetchers
+    ) {
         final BatchLoader<String, Map<String, String>> characterBatchLoader =
                 graphQLDataFetchers.characterBatchLoader();
-        final DataLoader<String, Map<String, String>> characterDataLoader =
-                DataLoader.newDataLoader(characterBatchLoader);
+
+        return DataLoader.newDataLoader(characterBatchLoader);
+    }
+
+    @Bean
+    public DataLoaderRegistry dataLoaderRegistry(
+            final DataLoader<String, Map<String, String>> authorDataLoader,
+            final DataLoader<String, Map<String, String>> characterDataLoader
+    ) {
+        final DataLoaderRegistry registry = new DataLoaderRegistry();
 
         registry.register("author", authorDataLoader);
         registry.register("character", characterDataLoader);
 
         return registry;
     }
+
 }
